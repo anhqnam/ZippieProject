f217754cb1b5c25ce890894527a89cf3664bb53f
VuongHuynh <huynhngocvuong91@gmail.com>
2014-10-15 15:48:28 +0700

    repair code

--- Zippie/src/com/lunextelecom/zippie/ZippieApplication.java
+++ Zippie/src/com/lunextelecom/zippie/ZippieApplication.java
@@ -1,22 +1,7 @@
 package com.lunextelecom.zippie;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Application;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class ZippieApplication.
- */
-public class ZippieApplication extends Activity {
+public class ZippieApplication extends Application {
 
-    /* (non-Javadoc)
-     * @see android.app.Activity#onCreate(android.os.Bundle)
-     */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-	// TODO Auto-generated method stub
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.welcome_screen);
-
-    }
 }
