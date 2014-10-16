c8ae2fbd0e999be596c2e13126b44ac35a49039f
AnhHa <hmanh.fit@gmail.com>
2014-10-16 11:33:09 +0700

    refs #13250 SignUp Feature - Create welcome screen

---
+++ Zippie/src/com/lunextelecom/zippie/fragment/InputPhoneNumberFragment.java
@@ -0,0 +1,7 @@
+package com.lunextelecom.zippie.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class InputPhoneNumberFragment extends Fragment {
+
+}
--- Zippie/src/com/lunextelecom/zippie/fragment/WelcomeScreenFragment.java
+++ Zippie/src/com/lunextelecom/zippie/fragment/WelcomeScreenFragment.java
@@ -1,12 +1,16 @@
 package com.lunextelecom.zippie.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -21,44 +25,63 @@ import com.lunextelecom.zippie.R;
  */
 public class WelcomeScreenFragment extends Fragment {
 
-    /** The m action bar. */
-    private ActionBar mActionBar;
+	/** The m action bar. */
+	private ActionBar mActionBar;
 
-    /** The m image view. */
-    private ImageView mImageView;
+	/** The m button start press. */
+	private ImageView mImageView, mButtonStart, mButtonStartPress;
 
-    private ImageView findViewById(int imagestart) {
-	// TODO Auto-generated method stub
-	return null;
-    }
+	/**
+	 * Find view by id.
+	 *
+	 * @param imagestart the imagestart
+	 * @return the image view
+	 */
+	private ImageView findViewById(int imagestart) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater,
-     * android.view.ViewGroup, android.os.Bundle)
-     */
-    @Override
-    public View onCreateView(LayoutInflater inflater,
-	    @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-	// TODO Auto-generated method stub
-	View view = inflater.inflate(R.layout.welcome_screen, null);
-	mImageView = (ImageView)view.findViewById(R.id.imageStart);
-	mActionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
-	mActionBar.hide();
-	
-	// Step1 : create the RotateAnimation object
-	RotateAnimation anim = new RotateAnimation(0, 360,
-		Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-		0.5f);
-	// Step 2: Set the Animation properties
-	anim.setInterpolator(new LinearInterpolator());
-	anim.setRepeatCount(Animation.INFINITE);
-	anim.setDuration(100000);
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater,
+	 * android.view.ViewGroup, android.os.Bundle)
+	 */
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.welcome_screen, null);
+		mButtonStart = (ImageView)view.findViewById(R.id.btnStart);
+		mButtonStart.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				FragmentManager fragmentManager = getFragmentManager();
+				FragmentTransaction transaction = fragmentManager.beginTransaction();
+				transaction.replace(R.id.fragment_container, new InputPhoneNumberFragment());
+				transaction.addToBackStack(null);
+				transaction.commit();
+			}
+		});
+		mImageView = (ImageView) view.findViewById(R.id.imageStart);
+		mActionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
+		mActionBar.hide();
 
-	// Step 3: Start animating the image
-	mImageView.startAnimation(anim);
-	return view;
-    }
+		// Step1 : create the RotateAnimation object
+		RotateAnimation anim = new RotateAnimation(0, 360,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		// Step 2: Set the Animation properties
+		anim.setInterpolator(new LinearInterpolator());
+		anim.setRepeatCount(Animation.INFINITE);
+		anim.setDuration(100000);
+
+		// Step 3: Start animating the image
+		mImageView.startAnimation(anim);
+		return view;
+	}
 }
