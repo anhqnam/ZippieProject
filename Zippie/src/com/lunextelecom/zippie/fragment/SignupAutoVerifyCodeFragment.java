b70b1fc2bd78e12145e5aa0bcc8f31f745248a8e
VuongHuynh <huynhngocvuong91@gmail.com>
2014-10-23 09:06:26 +0700

    refs #13254 SignUp Feature - Create Auto Verify code

---
+++ Zippie/src/com/lunextelecom/zippie/fragment/SignupAutoVerifyCodeFragment.java
@@ -0,0 +1,74 @@
+/*
+ * @author Vuong Huynh
+ * Copyright (C) 2014 Lunextelecom
+ */
+package com.lunextelecom.zippie.fragment;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lunextelecom.zippie.R;
+import com.lunextelecom.zippie.utils.Utils;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SignupAutoVerifyCodeFragment.
+ */
+public class SignupAutoVerifyCodeFragment extends Fragment implements OnClickListener{
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+     */
+    @SuppressLint("InflateParams") @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable
+            ViewGroup container, @Nullable
+            Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View view = inflater.inflate(R.layout.signup_auto_verify_code_lay, null);
+        TextView signUpHeaderVerifyTitleTv = (TextView)view.findViewById(R.id.signup_header_verify_title_id);
+        signUpHeaderVerifyTitleTv.setText(getResources().getString(R.string.signup_auto_verify_header_title_str));
+        ImageView signUpHeaderVerifyBackIv = (ImageView)view.findViewById(R.id.signup_header_verify_back_id);
+        ImageView signUpAutoVerifyCodeRuningIv = (ImageView)view.findViewById(R.id.signup_auto_verify_code_runing_id);
+        TextView signUpAutoVerifyNotifyTv = (TextView)view.findViewById(R.id.signup_auto_verify_notify_id);
+        TextView signUpAutoVerifyStartOverTv = (TextView)view.findViewById(R.id.signup_auto_verify_code_start_over_id);
+
+        signUpHeaderVerifyBackIv.setOnClickListener(this);
+        signUpAutoVerifyStartOverTv.setOnClickListener(this);
+
+        Utils.setTypefaceRoboto(getActivity(), signUpHeaderVerifyTitleTv, signUpAutoVerifyNotifyTv, signUpAutoVerifyStartOverTv);
+
+        signUpAutoVerifyCodeRuningIv.setBackgroundResource(R.drawable.signup_auto_verify_code_running);
+        AnimationDrawable frameAnimation = (AnimationDrawable) signUpAutoVerifyCodeRuningIv.getBackground();
+        frameAnimation.start();
+
+        return view;
+    }
+
+    /* (non-Javadoc)
+     * @see android.view.View.OnClickListener#onClick(android.view.View)
+     */
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        int id = v.getId();
+        switch (id) {
+            case R.id.signup_header_verify_back_id: //on click back on header
+
+                break;
+            case R.id.signup_auto_verify_code_start_over_id: //on click start over
+
+                break;
+            default:
+                break;
+        }
+    }
+}
